8b630beca14c4227a415c003aa9704a09fe87bb7
Cuevas072 <tachidazo@gmail.com>
2021-03-29 17:54:57 -0600

    Metodo de Euler en su forma final listo para implementar

--- Metodo_Euler/Metodo_Euler/MetodoEuler.cs
+++ Metodo_Euler/Metodo_Euler/MetodoEuler.cs
@@ -10,27 +10,29 @@ namespace Metodo_Euler
         double yn = 4;
         double h = 0;
         double y = 0;
-        double yr = 0;
+        double tope = 2f;
         int contador = 0;
 
         public void calcularEuler()
         {
-            while (xn < 2f)
+            while (xn < tope)
             {
                 double raiz = Math.Sqrt(y);
                 double denominador = (2 * xn) + 1;
+                double porcentajes;
                 y = yn + (h * (raiz / denominador));
-                yr = yn + (h * (raiz / denominador));
-                Math.Round(yr);
-                double ecuacion = Math.Pow((Math.Log((2 * xn) + 1) / 4) + 2, 2);
-                contador++;
                 Console.Write("Vuelta: " + contador + "El valor de la ecuacion es:");
-                Console.WriteLine(y);
-                Console.Write("El valor de la ecuacion chido es :");
-                Console.WriteLine(ecuacion);
+                Console.WriteLine("{0:N6}", y);
+                contador++;
                 xn = xn + h;
                 h = 0.25f;
                 yn = y;
+                double ecuacion = Math.Pow((Math.Log((2 * xn) + 1) / 4) + 2, 2);
+                Console.Write("El valor de la ecuacion chido es :");
+                Console.WriteLine("{0:N6}", ecuacion);
+                porcentajes = ((ecuacion - y) / ecuacion) * 100;
+                Console.Write("Porcentaje: ");
+                Console.WriteLine("{0:N1}", +Math.Abs(porcentajes));
             }
         }
     }
