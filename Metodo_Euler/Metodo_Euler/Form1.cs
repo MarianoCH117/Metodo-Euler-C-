d778fc36aff743facf5bc4984b43298f60fca147
CreamySweets <victorfc_1804@hotmail.com>
2021-03-29 19:32:06 -0600

    Adición de todas las columnas :0

--- Metodo_Euler/Metodo_Euler/Form1.cs
+++ Metodo_Euler/Metodo_Euler/Form1.cs
@@ -27,10 +27,35 @@ namespace Metodo_Euler
         private void Form1_Load(object sender, EventArgs e)
         {
             DataGridViewTextBoxColumn c1 = new DataGridViewTextBoxColumn();
-            c1.HeaderText = "Iteracion";
-            c1.Width = 100;
+            c1.HeaderText = "n";
+            c1.Width = 30;
             c1.ReadOnly = true;
+
+            DataGridViewTextBoxColumn c2 = new DataGridViewTextBoxColumn();
+            c2.HeaderText = "Xn";
+            c2.Width = 80;
+            c2.ReadOnly = true;
+
+            DataGridViewTextBoxColumn c3 = new DataGridViewTextBoxColumn();
+            c3.HeaderText = "Yreal";
+            c3.Width = 80;
+            c3.ReadOnly = true;
+
+            DataGridViewTextBoxColumn c4 = new DataGridViewTextBoxColumn();
+            c4.HeaderText = "Yeuler";
+            c4.Width = 80;
+            c4.ReadOnly = true;
+
+            DataGridViewTextBoxColumn c5 = new DataGridViewTextBoxColumn();
+            c5.HeaderText = "Ere";
+            c5.Width = 80;
+            c5.ReadOnly = true;
+
             dgvEuler.Columns.Add(c1);
+            dgvEuler.Columns.Add(c2);
+            dgvEuler.Columns.Add(c3);
+            dgvEuler.Columns.Add(c4);
+            dgvEuler.Columns.Add(c5);
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -75,7 +100,7 @@ namespace Metodo_Euler
                 porcentajes = ((ecuacion - y) / ecuacion) * 100;
                 //Console.Write("Porcentaje: ");
                 //Console.WriteLine("{0:N1}", +Math.Abs(porcentajes));
-                dgvEuler.Rows.Add(contador);
+                dgvEuler.Rows.Add(Convert.ToString(contador));
             }
 
             
