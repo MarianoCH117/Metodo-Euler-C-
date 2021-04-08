51565e2f288c3071e370e614a19a8d462bc17a4a
Cuevas072 <tachidazo@gmail.com>
2021-04-08 15:47:02 -0500

    Euler mejorado con Euler primeros cambios

--- Metodo_Euler/Metodo_Euler/Form1.cs
+++ Metodo_Euler/Metodo_Euler/Form1.cs
@@ -13,7 +13,7 @@ namespace Metodo_Euler
 {
     public partial class Form1 : Form
     {
-        
+
 
         public Form1()
         {
@@ -22,11 +22,12 @@ namespace Metodo_Euler
 
         private void label2_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
             DataGridViewTextBoxColumn c1 = new DataGridViewTextBoxColumn();
             c1.HeaderText = "n";
             c1.Width = 30;
@@ -52,11 +53,15 @@ namespace Metodo_Euler
             c5.Width = 80;
             c5.ReadOnly = true;
 
+
+
+
             dgvEuler.Columns.Add(c1);
             dgvEuler.Columns.Add(c2);
             dgvEuler.Columns.Add(c3);
             dgvEuler.Columns.Add(c4);
             dgvEuler.Columns.Add(c5);
+
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -77,6 +82,7 @@ namespace Metodo_Euler
         private void btnCalcularEuler_Click(object sender, EventArgs e)
         {
             calcularEuler();
+            calcularEulerMejorado();
         }
 
         private void txtMaxX_KeyPress(object sender, KeyPressEventArgs e)
@@ -107,6 +113,7 @@ namespace Metodo_Euler
             if (e.KeyChar == (char)Keys.Enter)
             {
                 calcularEuler();
+                calcularEulerMejorado();
                 e.Handled = true;
             }
         }
@@ -123,6 +130,7 @@ namespace Metodo_Euler
                 e.Handled = true;
             }
         }
+
         private void calcularEuler()
         {
             this.dgvEuler.Rows.Clear();
@@ -168,5 +176,82 @@ namespace Metodo_Euler
                 }
             }
         }
+
+        private void calcularEulerMejorado()
+        {
+            DataGridViewTextBoxColumn c6 = new DataGridViewTextBoxColumn();
+            c6.HeaderText = "YEulerMejorado";
+            c6.Width = 80;
+            c6.ReadOnly = true;
+
+            DataGridViewTextBoxColumn c7 = new DataGridViewTextBoxColumn();
+            c7.HeaderText = "PorcentajeMejorado";
+            c7.Width = 80;
+            c7.ReadOnly = true;
+
+            dgvEuler.Columns.Add(c6);
+            dgvEuler.Columns.Add(c7);
+
+            if (txtH.Text == "" || txtMaxX.Text == "" || txtXo.Text == "" || txtYo.Text == "")
+            {
+                DialogResult error = MessageBox.Show("Por favor, revise que no haya ninguna caja de texto en blanco.", "Error de datos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                double x0 = 0;
+                double y0 = 0;
+                double h = 0;
+                double tope = 0f;
+                double y = 0;
+                double yn = 0;
+                double xn = 0;
+                double ecua;
+                double ecuaum;
+                try
+                {
+                    x0 = double.Parse(txtXo.Text);
+                    y0 = double.Parse(txtYo.Text);
+                    h = 0;
+                    y = 0;
+                    tope = double.Parse(txtMaxX.Text);
+
+                    while (xn < tope)
+                    {
+                        double porcentajes;
+                        double raiz = (double)Math.Sqrt(y0);
+                        double denominador = (2 * x0) + 1;
+                        ecua = raiz / denominador;
+                        yn = (y0 + (h * ecua));
+                        xn = x0 + h;
+                        double raizaum = (double)Math.Sqrt(yn);
+                        double denominadoraum = (2 * xn) + 1;
+                        ecuaum = raizaum / denominadoraum;
+                        y = y0 + (h / 2) * (ecua + ecuaum);
+                        double yreal = (double)Math.Pow((Math.Log((2 * xn) + 1) / 4) + 2, 2);
+                        h = double.Parse(txtH.Text, CultureInfo.InvariantCulture);
+                        porcentajes = Math.Abs(((yreal - y) / yreal) * 100);
+                        x0 = xn;
+                        y0 = y;
+
+                        dgvEuler.Rows.Add(
+                            Convert.ToString(Math.Round(y, 6, MidpointRounding.AwayFromZero)),
+                            Convert.ToString(Math.Round(porcentajes, 6, MidpointRounding.AwayFromZero)) + "%");
+                    }
+
+                }
+                catch (FormatException)
+                {
+                    DialogResult error = MessageBox.Show("Error en el formato de los datos introducidos. Por favor, introduzca sólo números.", "Error de datos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void llenado(double n, double xn, double yreal, double Ere, double ynem, double erem)
+        {
+
+
+        }
+
+
     }
 }
